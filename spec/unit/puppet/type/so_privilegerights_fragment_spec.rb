96cdcc561b9a5fc5a55ddb95febb191d16957e3f
johnrogers00 <totheo@gmail.com>
2020-11-18 07:30:31 +0100

    added another check for validating fragment name

--- spec/unit/puppet/type/so_privilegerights_fragment_spec.rb
+++ spec/unit/puppet/type/so_privilegerights_fragment_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
 describe Puppet::Type.type(:so_privilegerights_fragment) do
-    let(:valid_right) { 'seassignprimarytokenprivilege' }
+  let(:valid_right) { 'seassignprimarytokenprivilege' }
+  let(:invalid_name) { 'Manage invalide privilege right name' }
 
-    context 'when validating right' do
-        it 'should accept a valid string' do
-            res = described_class.new(:title => 'abc', :right => valid_right)
-            expect(res[:right]).to eq(valid_right)
-        end
-
-        # it 'should fail with an invalid right' do
-        #    expect {
-        #         described_class.new(
-        #             :title => 'abc',
-        #            :right => 'abc1',
-        #        )
-        #    }.to raise_error(Puppet::Error, /Not a valid right name: 'abc1'/)
-        #end
+  context 'when validating right' do
+    it 'should accept a valid string' do
+      res = described_class.new(:title => 'abc', :right => valid_right)
+      expect(res[:right]).to eq(valid_right)
     end
+    it 'fails with an invalid name' do
+      expect {
+        described_class.new(
+          name: invalid_name,
+          right: invalid_name,
+        )
+      }.to raise_error(Puppet::ResourceError, %r{Invalid name: \'Manage invalide privilege right name\'})
+    end
+  end
 end
