fbd35b8029cda5dca266941e5a4def65f0688e1c
John Rogers <totheo@gmail.com>
2020-11-24 16:09:36 +0100

    Add prfragments type spec (#14)
    * remove unneeded puts statement
    
    * updated output fixture to have the privilege rights sections
    
    * removed extra fixture files and added the so_privilegerights provider spec file
    
    * added test for fragments
    
    * added another check for validating fragment name
    
    * fixed lint errors
    
    * fixed validate errors

---
+++ spec/unit/puppet/type/so_privilegerights_fragment_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:so_privilegerights_fragment) do
+  let(:valid_right) { 'seassignprimarytokenprivilege' }
+  let(:invalid_name) { 'Manage invalide privilege right name' }
+
+  context 'when validating right' do
+    it 'accepts a valid string' do
+      res = described_class.new(title: 'abc', right: valid_right)
+      expect(res[:right]).to eq(valid_right)
+    end
+    it 'fails with an invalid name' do
+      expect {
+        described_class.new(
+          name: invalid_name,
+          right: invalid_name,
+        )
+      }.to raise_error(Puppet::ResourceError, %r{Invalid name: \'Manage invalide privilege right name\'})
+    end
+  end
+end
